0c00deec42747f6ea5d72d96df23ec08a6445835
Divana Nayumi Lucky Pratama <divanayumi13@gmail.com>
2021-03-19 09:14:41 +0700

    Pengerjaan soal

--- I0320028_soal1_tugas3.py
+++ I0320028_soal1_tugas3.py
@@ -1,10 +1,10 @@
-# Menampilkan list 10 nama teman
+# Membuat list 10 nama teman
 list = ['Ara', 'Gea', 'Hanna', 'Candrika', 'Funny', 'Ayu', 'Angela', 'Alifiana', 'Dhea', 'Deadila']
 
 # Menampilkan isi list indeks nomor 4, 6, dan 7
-print("List indeks nomor 4, 6, dan 7 yaitu", list[4], ",", list[6], ",", "dan", list[7])
+print("List indeks nomor 4, 6, dan 7 adalah", list[4], ",", list[6], ",", "dan", list[7])
 
-# Mengganti nama teman di list 3, 5, dan 9
+# Mengganti nama teman yang berada di list 3, 5, dan 9
 list[3] = 'Vizal'
 list[5] = 'Jefri'
 list[9] = 'Diva'
@@ -13,8 +13,11 @@ list[9] = 'Diva'
 list.append('Issa')
 list.append('Hani')
 
-# Menampilkan semua teman dengan pengulangan
-print(list*2)
+# Menampilkan semua nama teman dengan pengulangan
+black = 0
+for blue in range (0,12) :
+    print (list[black])
+    black+=1
 
 # Menampilkan panjang list
-print(len(list))
+print("Panjang list nama teman yaitu", len(list))
--- I0320028_soal2_tugas3.py
+++ I0320028_soal2_tugas3.py
@@ -1,11 +1,15 @@
-# Membuat dictionary pada python
+# Membuat Dictionary pada Python
 dict = {'Nama':'Divana','Hobi1':'Berenang','Hobi2':'Membaca novel','Hobi3':'Kulineran','Sosmed1':'twitter:DivanaNayumiLP','Sosmed2':'line:nayumi_13','Sosmed3':'ig:divanaaayumi','Lagukesukaan1':'Imagination','Lagukesukaan2':'Perfect','Lagukesukaan3':'To The Bone','Makananfavorit1':'Mie ayam','Makananfavorit2':'Steak','Makananfavorit3':'Nasi goreng'}
 
-# Mengubah salah satu dari hobi dan sosmed, menghapus 2 makanan favorit, dan menambah 1 hobi
+# Mengubah salah satu dari hobi dan sosial media
 dict['Hobi3'] = 'Jalan-jalan'
 dict['Sosmed1'] = 'twitter:Divanayumi'
+
+# Menghapus 2 makanan favorit
 del dict['Makananfavorit2']
 del dict['Makananfavorit3']
+
+# Menambahkan 1 hobi
 dict['Hobi4'] = 'Mendengarkan musik'
 
 print(dict)
